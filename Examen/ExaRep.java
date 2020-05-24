bef22ab4be699f799bc8d4df10b591eeb8741120
repl.it user <replituser@example.com>
2020-05-24 16:28:59 +0000

    archivo J

---
+++ Examen/ExaRep.java
@@ -0,0 +1,14 @@
+public class tipvacMLI{
+ public static void vacuna(int edad)
+ {
+   if(edad>70){
+     System.out.print("aplicar vacuna C");
+   }else if
+   (edad>=16 && edad<=69 && genero="M");{
+     System.out.print("aplicar vacuna B");
+   }else if
+    System.out.print("aplicar vacuna A");
+ }
+}
+public class static void main(stryng[]args) {
+  vacuna()}
